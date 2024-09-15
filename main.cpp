2ce8a47f86995d6f726453a35aea1b8c89be8827
Luisa fernandez <luisa.fernandez2@udea.edu.com>
2024-09-14 21:11:45 -0500

    Actualización Pulsador

--- main.cpp
+++ main.cpp
@@ -1,55 +1,55 @@
+
 #include <LiquidCrystal.h>
 
 int seconds = 0;
 int analogPin = 0;
 int val = 0;
-int Pulsador = 7;
 int Estado_Pul = 0;
-int even_time= 0;
-bool Suceso_time = false;
+unsigned long tiempoInicio = 0;
+unsigned long tiempoFin = 0;
+unsigned long tiempoPresionado = 0;
+
 
 LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
 
-unsigned long Tiempo(){
-    
-    pinMode(Pulsador, INPUT);
-    
-  Estado_Pul = digitalRead(Pulsador);
-    
-    if ( Estado_Pul == LOW && !Suceso_time ){
-        
-       even_time = millis(); // Devuelve el tiempo en milisegundos desde que Arduino comenzó
-        Suceso_time = true;
-    }
-    if ( Suceso_time) {
-      
-      unsigned long Intervalo_tiempo = millis() - even_time;
-      return Intervalo_tiempo; 
-        
-    }
-   return 0; 
-}
-
 void setup()
 {
-  Serial.begin(9600);
-  lcd_1.begin(16, 2);
-  lcd_1.print("Odio esto");
+    Serial.begin(9600);
+    lcd_1.begin(16, 2);
+    lcd_1.print("Hola_mundo");
 }
 
 void loop()
 {
-  val = analogRead(analogPin);
-  Serial.println(val);
-  lcd_1.setCursor(0, 1);
-  lcd_1.print(seconds);
-  delay(1000);
-  seconds += 1;
-  
-  unsigned long Tiempo_obtenido = Tiempo();
-  if (Tiempo_obtenido != 0) {
-        
-        Serial.println(Tiempo_obtenido);
+    val = analogRead(analogPin);
+    Serial.println(val);
+    lcd_1.setCursor(0, 1);
+    lcd_1.print(seconds);
+    delay(1000);
+    seconds += 1;
+    Tiempo();
+}
 
+unsigned long Tiempo(){
+
+    pinMode(7, INPUT);
+
+
+    Estado_Pul = digitalRead(7);
+    if (Estado_Pul == HIGH) {
+        if (tiempoInicio == 0) { // Si es la primera vez que se presiona
+
+            tiempoInicio = millis();
+        }
+    } else {
+        if (tiempoInicio != 0) { // Si se había presionado antes
+            tiempoFin = millis(); // Guarda el tiempo de fin
+            tiempoPresionado = tiempoFin - tiempoInicio;
+            Serial.print("Tiempo presionado: ");
+            Serial.print(tiempoPresionado);
+            Serial.println(" ms");
+
+            tiempoInicio = 0; // Resetea el tiempo de inicio para la próxima vez
+        }
     }
 }
