ea8c964f45c37171918006dc3866d68c40845df7
Stiben Ramos <aldair.ramos@udea.edu.co>
2024-09-16 23:27:16 -0500

    Se hicieron correciones en la configuración del arduino y el calculo de amplitud y frecuencia

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <LiquidCrystal.h>
+
+unsigned long tiempoInicio;
+unsigned long tiempoTranscurrido;
 int funGenerador = 0;
+float maxVal = -1023;
+float minVal = 1023;
+float amplitud;
+float periodo;
+float frecuencia;
 int pul_1 = 13;
 int pul_2 = 7;
 int ledPul = 8;
@@ -7,9 +15,6 @@ int val = 0;
 int estPul_1 = LOW;
 int estPul_2 = LOW;
 int estLed = LOW;
-unsigned long tiempoInicio = 0;
-unsigned long tiempoFin = 0;
-unsigned long tiempoPresionado = 0;
 
 
 LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
@@ -21,47 +26,53 @@ void setup()
     pinMode(ledPul, OUTPUT);
     Serial.begin(9600);
     lcd_1.begin(16, 2);
+    tiempoInicio = millis();
 }
 
 void loop()
 {
     val = analogRead(funGenerador);
-    Serial.println(val);
+    if(val > maxVal){
+        maxVal = val;
+    }
+    if (val < minVal){
+        minVal = val;
+    }
+    amplitud = (maxVal - minVal) * (5.0/1023.0);
+
+    if (val > 0) {
+        tiempoTranscurrido = millis();
+        periodo = tiempoTranscurrido - tiempoInicio;
+        frecuencia = 1000.0 / periodo;
+    }
+
+    Serial.print(val);
+    Serial.print(", A:");
+    Serial.print(amplitud);
+    Serial.print(", F :");
+    Serial.println(frecuencia);
     estPul_1 = digitalRead(pul_1);
     estPul_2 = digitalRead(pul_2);
-
-    lcd_1.setCursor(0, 0);
-    lcd_1.print("Tiempo: ");
+    lcd_1.blink();
 
     if(digitalRead(pul_1) == HIGH){
         digitalWrite(ledPul, HIGH);
         if(digitalRead(ledPul) == HIGH){
-            lcd_1.print(Tiempo());
-            lcd_1.print(" ms");
-                //Comenzar a capturar datos
+            lcd_1.clear();
+            lcd_1.setCursor(0, 0);
+            lcd_1.print("Capturando...");
+            lcd_1.setCursor(0, 1);
+            //Comenzar a capturar datos
         }
     }
     if(digitalRead(pul_2) == HIGH){
         digitalWrite(ledPul, LOW);
         if(digitalRead(ledPul) == LOW){
+            lcd_1.clear();
+            lcd_1.setCursor(0, 0);
+            lcd_1.print("Tiempo: ");
+            lcd_1.print(" ms");
             //Procesar los datos
         }
     }
-}
-
-unsigned long Tiempo(){
-    delay(1000);
-    estPul_1 = digitalRead(pul_1);
-    if (estPul_1 == HIGH) {
-        if (tiempoInicio == 0) { // Si es la primera vez que se presiona
-            tiempoInicio = millis();
-        }
-    } else {
-        if (tiempoInicio != 0) { // Si se había presionado antes
-            tiempoFin = millis(); // Guarda el tiempo de fin
-            tiempoPresionado = tiempoFin - tiempoInicio;
-            tiempoInicio = 0; // Resetea el tiempo de inicio para la próxima vez
-        }
-    }
-    return tiempoPresionado;
 }
