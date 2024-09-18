335eecbaeeb0b343c3ef361e1966dec458a85e47
Stiben Ramos <aldair.ramos@udea.edu.co>
2024-09-17 23:48:24 -0500

    Se trabajo en los pulsadores para que comiencen a calcular los datos o dejende hacerlo

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 unsigned long tiempoInicio;
 unsigned long tiempoTranscurrido;
 int funGenerador = 0;
-float maxVal = -1023;
+float maxVal = 0;
 float minVal = 1023;
-float amplitud;
-float periodo;
-float frecuencia;
+float amplitud; // variable decimales
+float periodo; // decimales
+float frecuencia; // decimales
 int pul_1 = 13;
 int pul_2 = 7;
 int ledPul = 8;
@@ -15,6 +15,18 @@ int val = 0;
 int estPul_1 = LOW;
 int estPul_2 = LOW;
 int estLed = LOW;
+bool capturando = false; // Variable para saber si estamos capturando datos
+
+// Punteros
+
+float *pun_maxVal = &maxVal;
+float *pun_minVal = &minVal;
+
+// memoria
+
+int Tam = 20; // numero de espacios para los datos de la señal.
+int *pun_men = new int[Tam];
+int pos_arre = 0;  // registro de la posición actual en el arreglo donde se debe almacenar el próximo valor. en este caso la primera posición del arreglo comienza en 0.
 
 
 LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
@@ -31,48 +43,67 @@ void setup()
 
 void loop()
 {
-    val = analogRead(funGenerador);
-    if(val > maxVal){
-        maxVal = val;
-    }
-    if (val < minVal){
-        minVal = val;
-    }
-    amplitud = (maxVal - minVal) * (5.0/1023.0);
-
-    if (val > 0) {
-        tiempoTranscurrido = millis();
-        periodo = tiempoTranscurrido - tiempoInicio;
-        frecuencia = 1000.0 / periodo;
-    }
-
-    Serial.print(val);
-    Serial.print(", A:");
-    Serial.print(amplitud);
-    Serial.print(", F :");
-    Serial.println(frecuencia);
     estPul_1 = digitalRead(pul_1);
     estPul_2 = digitalRead(pul_2);
-    lcd_1.blink();
+    estLed = digitalRead(ledPul);
 
-    if(digitalRead(pul_1) == HIGH){
+    if(estPul_1 == HIGH){
+        capturando = true;
         digitalWrite(ledPul, HIGH);
-        if(digitalRead(ledPul) == HIGH){
-            lcd_1.clear();
-            lcd_1.setCursor(0, 0);
-            lcd_1.print("Capturando...");
-            lcd_1.setCursor(0, 1);
-            //Comenzar a capturar datos
-        }
     }
-    if(digitalRead(pul_2) == HIGH){
+    if(estPul_2 == HIGH){
+        capturando = false;
         digitalWrite(ledPul, LOW);
-        if(digitalRead(ledPul) == LOW){
-            lcd_1.clear();
-            lcd_1.setCursor(0, 0);
-            lcd_1.print("Tiempo: ");
-            lcd_1.print(" ms");
-            //Procesar los datos
+    }
+
+    if (digitalRead(ledPul) == HIGH){
+        lcd_1.clear();
+        lcd_1.setCursor(0, 0);
+        lcd_1.print("Onda...");
+        lcd_1.setCursor(0, 1);
+        lcd_1.print(frecuencia);
+        lcd_1.print(" Hz");
+        lcd_1.setCursor(10, 1);
+        lcd_1.print(amplitud);
+        lcd_1.print(" V");
+        delay(100);
+    }
+
+    if(digitalRead(ledPul) == LOW){
+        lcd_1.clear();
+        lcd_1.setCursor(0, 0);
+        lcd_1.print("Esperando...");
+        *pun_maxVal = 0;
+        *pun_minVal = 1023;
+        float amplitud = 0;
+        float periodo = 0;
+        float frecuencia = 0;
+        delay(100);
+    }
+
+    if(capturando){
+        val = analogRead(funGenerador);
+        pun_men[pos_arre] = val;
+        pos_arre = (pos_arre + 1) % Tam;
+
+        if(val > *pun_maxVal){
+            *pun_maxVal = val;
         }
+        if (val < *pun_minVal){
+            *pun_minVal = val;
+        }
+        amplitud= (maxVal - minVal) * (5.0/1023.0);
+
+        if (val > 0) {
+            tiempoTranscurrido = millis();
+            periodo = tiempoTranscurrido - tiempoInicio;
+            frecuencia = 1000.0 / periodo;
+            tiempoInicio = tiempoTranscurrido;
+        }
+        Serial.print(val);
+        Serial.print(", A:");
+        Serial.print(amplitud);
+        Serial.print(", F :");
+        Serial.println(frecuencia);
     }
 }
